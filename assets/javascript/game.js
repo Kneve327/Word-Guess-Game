2c0c81403eac1a286599fb523b26633aa856b90d
Kneve327 <kneve327@gmail.com>
2018-09-15 08:51:21 -0400

    push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var wordsToGuessId = document.getElementById("wordsToGuess");
 var lettersGuessedId = document.getElementById("lettersGuessed");
-var winsId = document.getElementById("wins");
+
 var lossesId = document.getElementById("losses");
 var triesLeftId = document.getElementById("triesLeft");
 var startButtonId = document.getElementById("startButton");
@@ -9,11 +9,14 @@ var wins = 0;
 var losses = 0;
 var incorrectGuesses = [];
 var correctGuesses = [];
+var randomWord = ".";
 var triesLeft = 15;
-var randomWord = " ";
-var randomWordUnderscores = " ";
+var randomWordUnderscores = [];
 var wordArray = ["Costa Rica", "Honduras", "England", "Ireland", "Italy", "Greece", "Puerto Rico", "Bahamas", "Jamaica", "Hawaii", "France", "Wales", "India", "Morocco", "Spain", "New Zealand", "Canada", "Switzerland", "Australia"];
 var gameRunning = false;
+var gameOver = false;
+
+document.getElementById("wins").innerHTML = wins;
 
 
 function newGame() {
@@ -22,28 +25,34 @@ function newGame() {
     correctGuesses = [];
     incorrectGuesses = [];
     randomWordUnderscores = [];
+    randomWord = "";
 
     randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-
     for (var i = 0; i < randomWord.length; i++) {
+        
+        console.log(gameOver);
         if (randomWord[i] === ' ') {
             randomWordUnderscores.push(' ');
         } else {
             randomWordUnderscores.push('_');
         }
     }
-
     triesLeftId.textContent = triesLeft;
     wordsToGuessId.textContent = randomWordUnderscores.join('');
     lettersGuessedId.textContent = incorrectGuesses;
+    
 }
 
 function letterGuess(letter) {
     console.log(letter);
 
-    if (gameRunning === true && correctGuesses.indexOf(letter) === -1) {
+    if ((gameRunning === true) && (correctGuesses.indexOf(letter) === -1)) {
 
-        correctGuesses.push(letter);
+        if (randomWord.indexOf(letter) > -1) {
+            correctGuesses.push(letter);
+        }
+
+        console.log(correctGuesses);
 
         for (var i = 0; i < randomWord.length; i++) {
 
@@ -54,49 +63,57 @@ function letterGuess(letter) {
         }
 
         wordsToGuessId.textContent = randomWordUnderscores.join('');
-
+        if (randomWordUnderscores.indexOf("_") === -1) {
+            gameOver = true;
+            console.log("We just won")
+        }
         confirmLetter(letter);
-    }
 
+    }
 }
 
 
 
 function confirmLetter(letter) {
 
-    if (randomWordUnderscores.indexOf(letter.toLowerCase()) === -1 && randomWordUnderscores.indexOf(letter.toUpperCase()) === -1) {
+    if (randomWord.indexOf(letter.toLowerCase()) === -1 && randomWordUnderscores.indexOf(letter.toUpperCase()) === -1) {
 
         triesLeft--;
+
+        if (randomWord.indexOf(letter) > -1) {
+            incorrectGuesses.push(letter);
+        }
 
         incorrectGuesses.push(letter);
 
         lettersGuessedId.textContent = incorrectGuesses.join(' ');
 
         triesLeftId.textContent = triesLeft;
+        
     }
     
 }
+//if correct guesses = word the win
 
-function loseGame() {
-    if (triesLeft === 0) {
+
+function gameScore() {
+    if (triesLeft < 1) {
         losses++;
         gameRunning = false;
         lossesId.textContent = losses;
         wordsToGuessId.textContent = randomWord;
-    }  
-}
-
-loseGame();
-
-function winGame() {
-    if (randomWord.toLowerCase() === randomWordUnderscores.toLowerCase()) {
+    } else if (gameOver) {
+    //} else if (randomWord.toLowerCase() === (correctGuesses.toString()).toLowerCase()) {
+        
         wins++;
         gameRunning = false;
-        winsId.textContent = wins;
+        document.getElementById("wins").innerHTML = "Wins: " + wins;
     }
+    
+ 
 }
 
-winGame();
+
 
 startButton.addEventListener("click", newGame);
 
@@ -105,5 +122,5 @@ document.onkeyup = function (event) {
     if (event.keyCode >= 65 && event.keyCode <= 90) {
         letterGuess(event.key);
     }
+    
 }
-
